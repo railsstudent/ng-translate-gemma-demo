47e1bf5762a79c25d6bec47bae45fb31de742aad
Connie Leung <cleung334@gmail.com>
2026-02-22 00:51:49 +0800

    feat(ui): Integrate FooterComponent into AppComponent

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,15 +1,21 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { NgIcon, provideIcons } from '@ng-icons/core';
-import { bootstrapGithub, bootstrapTranslate } from '@ng-icons/bootstrap-icons'
+import { bootstrapGithub, bootstrapTranslate } from '@ng-icons/bootstrap-icons';
+import { FooterComponent } from './footer/footer.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, NgIcon],
+  imports: [RouterOutlet, NgIcon, FooterComponent],
   viewProviders: [provideIcons({ bootstrapGithub, bootstrapTranslate })],
   template: `
-    <div>Hello {{ title() }}<ng-icon name="bootstrapGithub" /><ng-icon name="bootstrapTranslate" /></div>
-    <router-outlet />
+    <div class="flex flex-col min-h-screen">
+      <main class="flex-1">
+        <div>Hello {{ title() }}<ng-icon name="bootstrapGithub" /><ng-icon name="bootstrapTranslate" /></div>
+        <router-outlet />
+      </main>
+      <app-footer />
+    </div>
   `,
 })
 export class App {
